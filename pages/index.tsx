d5f6c0c9791680275eb2b79a98e1f2643dbeb887
Antônio Bernardino da Silva <antoniobsilvaprof@hotmail.com>
2020-09-15 23:23:03 -0300

    1.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <Grid
       as="main"
       height="100vh"
-      templateColumns="1fr 480px 480px 1fr"
+      templateColumns="1fr 400px 400px 1fr"
       templateRows="1fr 480px 1fr"
       templateAreas="
     '. . . .'
